6470459a0706168986b57f8f78559dadad62277d
Rakshith Sindhiya R <rakshithsindhiyar@Rakshiths-MacBook-Pro.local>
2024-02-17 17:49:38 +0530

    fix: auth context fix & refactored code

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,11 +1,10 @@
 import { Navigate, Outlet } from "react-router-dom";
-// import { useAuth } from "../common/slices";
 import { ROUTES } from "../constants";
 import { useContext } from "react";
 import { AuthContext } from "../context/authContext";
 
 const ProtectedRoute = () => {
   const { user } = useContext(AuthContext);
-  return user ? <Outlet /> : <Navigate to={ROUTES.LOGIN} replace />;
+  return user?.userId ? <Outlet /> : <Navigate to={ROUTES.LOGIN} replace />;
 };
 export default ProtectedRoute;
